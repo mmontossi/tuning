3020c31572a9b6e9172c4ec308d856fa288c28c6
Mathías Montossi <mmontossi@mattways.com>
2013-04-10 19:53:07 -0300

    A few more tweaks

--- lib/rails_contrib/action_view/base.rb
+++ lib/rails_contrib/action_view/base.rb
@@ -2,6 +2,11 @@ module RailsContrib
   module ActionView
     module Base
 
+      def self.included(base)
+        base.alias_method_chain :submit_tag, :button
+        base.alias_method_chain :form_for, :defaults
+      end
+
       def conditional_tag(tag, condition, options=nil, &block)
         condition ? content_tag(tag, options, &block) : capture(&block)
       end
@@ -14,6 +19,15 @@ module RailsContrib
         end
       end
 
+      def submit_tag_with_button(value='Send', options={})
+        button_tag({ :type => 'submit', :name => 'commit' }.update(options)) { content_tag(:span, value) }
+      end
+
+      def form_for_with_defaults(record, options={}, &proc)
+        options.reverse_merge! :url => request.fullpath, :method => :post
+        form_for_without_defaults record, options, &proc
+      end
+      
     end
   end
 end
