58e7396f52835e15cc87e6859e51b543394b700e
Mathías Montossi <mmontossi@mattways.com>
2013-04-11 14:01:00 -0300

    Remove form_for defaults

--- lib/rails_contrib/action_view/base.rb
+++ lib/rails_contrib/action_view/base.rb
@@ -4,7 +4,6 @@ module RailsContrib
 
       def self.included(base)
         base.alias_method_chain :submit_tag, :button
-        base.alias_method_chain :form_for, :defaults
       end
 
       def conditional_tag(tag, condition, options=nil, &block)
@@ -21,11 +20,6 @@ module RailsContrib
 
       def submit_tag_with_button(value='Send', options={})
         button_tag({ :type => 'submit', :name => 'commit' }.update(options)) { content_tag(:span, value) }
-      end
-
-      def form_for_with_defaults(record, options={}, &proc)
-        options.reverse_merge! :url => request.fullpath, :method => :post
-        form_for_without_defaults record, options, &proc
       end
       
     end
