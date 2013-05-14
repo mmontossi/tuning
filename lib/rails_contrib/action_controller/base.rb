43925d3fe1965affbb97735fcfd48484ffa3f9d4
Mathías Montossi <mmontossi@mattways.com>
2013-05-14 01:34:33 -0300

    Remove increment_cache_namespace and use flash.now in flash_errors

--- lib/rails_contrib/action_controller/base.rb
+++ lib/rails_contrib/action_controller/base.rb
@@ -32,14 +32,14 @@ module RailsContrib
       end
 
       def flash_errors(source)
-        flash[:error] = [] unless flash[:error].is_a? Array
+        flash.now[:error] = [] unless flash.now[:error].is_a? Array
         case source
         when String
-          flash[:error] << source
+          flash.now[:error] << source
         when Array
-          source.each { |error| flash[:errors] << error }
+          source.each { |error| flash.now[:error] << error }
         else
-          source.errors.full_messages.each { |error| flash[:error] << error }
+          source.errors.full_messages.each { |error| flash.now[:error] << error }
         end
       end
 
