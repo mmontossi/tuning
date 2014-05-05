1cc9400ec2d1cb6ad973a51a1725ce1c9ca138c5
Mathías Montossi <mmontossi@museways.com>
2014-05-05 18:53:54 -0300

    Fix assert_not error with rails 3

--- test/active_trail_test.rb
+++ test/active_trail_test.rb
@@ -18,10 +18,10 @@ class ActiveTrailTest < ActionView::TestCase
 
   test "should return false if path is not in active trail" do
     set_path '/'
-    assert_not active_trail?('/some-path')
+    assert !active_trail?('/some-path')
 
     set_path '/some-path'
-    assert_not active_trail?('/other-path')
+    assert !active_trail?('/other-path')
   end
 
   private
